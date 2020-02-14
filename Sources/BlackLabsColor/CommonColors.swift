ad39f9aeddfadffa8ba06a5b2e36d1f5bbbff231
Norman Basham <nbasham@yahoo.com>
2020-02-14 07:28:12 -0800

    Made common colors public.

--- Sources/BlackLabsColor/CommonColors.swift
+++ Sources/BlackLabsColor/CommonColors.swift
@@ -1,6 +1,6 @@
 import UIKit
 
-extension UIColor {
+public extension UIColor {
     static var appWhite: UIColor { return UIColor(red: 245.0/255.0, green: 245.0/255.0, blue: 245.0/255.0, alpha: 1.0) }
     static var appBlack: UIColor { return UIColor(red: 30.0/255.0, green: 30.0/255.0, blue: 30.0/255.0, alpha: 1.0) }
     static var iOS7Blue: UIColor { return UIColor(red: 0.0, green: 122.0/255.0, blue: 1.0, alpha: 1.0) }
